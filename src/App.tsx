a705dddc9f620eb1ea477d932ac19fe4c4b8c779
E.Sherstnev <egor.sherstnev@immelman.ru>
2024-07-30 18:15:14 +0300

    added css module settings

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Counter from './components/Counter';
+import './index.scss'
+
+const App = () => {
+  return (
+    <div className='app'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, modi explicabo aut corrupti praesentium ipsum aperiam. Pariatur hic facilis, quam dolorem esse illum alias voluptate, placeat possimus nostrum voluptatum quidem?
+        <Counter />
+    </div>
+  )
+}
+
+export default App;
