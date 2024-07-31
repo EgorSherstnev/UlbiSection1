6a634ab15e4e40fba950971601bedf3cba434872
E.Sherstnev <egor.sherstnev@immelman.ru>
2024-07-31 18:40:19 +0300

    section-1 lesson-6 added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { Route, Routes, Link } from 'react-router-dom';
 import Counter from './components/Counter';
 import './index.scss'
+import AboutPage from './pages/AboutPage/AboutPage';
+import MainPage from './pages/MainPage/MainPage';
 
 const App = () => {
   return (
     <div className='app'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae, modi explicabo aut corrupti praesentium ipsum aperiam. Pariatur hic facilis, quam dolorem esse illum alias voluptate, placeat possimus nostrum voluptatum quidem?
-        <Counter />
+      <Link to={'/'}>Главная</Link>
+      <Link to={'/about'}>О сайте</Link>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/about' element={<AboutPage />} />
+        {/* <Route path='/contact' element={<Contact />} /> */}
+        {/* <Route path='*' element={<NotFound />} /> */}
+      </Routes>
     </div>
   )
 }
