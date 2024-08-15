7edf5bdc855367b4a8c64ac9187cadf811364854
E.Sherstnev <egor.sherstnev@immelman.ru>
2024-08-15 09:44:41 +0300

    section-1 lesson-10 addded index files for pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
 import { Route, Routes, Link } from 'react-router-dom';
 import './styles/index.scss'
 import { Suspense, useContext, useState } from 'react';
-import { classNames } from 'helpers/classsNames/classNames';
-import { MainPageAsync } from 'pages/MainPage/MainPage.async';
-import { AboutPageAsync } from 'pages/AboutPage/AboutPage.async';
+import { classNames } from 'shared/lib/classsNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
+import { AboutPage } from 'pages/AboutPage';
+import { MainPage } from 'pages/MainPage';
 
 const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -16,10 +16,8 @@ const App = () => {
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
-          <Route path='/' element={<MainPageAsync />} />
-          <Route path='/about' element={<AboutPageAsync />} />
-          {/* <Route path='/contact' element={<Contact />} /> */}
-          {/* <Route path='*' element={<NotFound />} /> */}
+          <Route path='/' element={<MainPage />} />
+          <Route path='/about' element={<AboutPage />} />
         </Routes>
       </Suspense>
     </div>
