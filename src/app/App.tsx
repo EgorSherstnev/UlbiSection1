7d39ed3cfd3398380fe80333d78699bb4bcaacad
E.Sherstnev <egor.sherstnev@immelman.ru>
2024-08-30 16:59:36 +0300

    secction-2 lesson-15 added i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,17 +8,24 @@ import { MainPage } from 'pages/MainPage';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
+import { useTranslation } from 'react-i18next';
+
+const Component = () => {
+
+}
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
     <div className= {classNames('app', {}, [theme])}>
-      <Navbar />
-      <div className='content-page'>
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback="">
+        <Navbar />
+        <div className='content-page'>
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   )
 }
